9df9ce1476083926198d3ba6b87e1ed91a20f46c
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-11-11 12:06:52 +0100

    derived metrics distributions

--- scripts/vc.py
+++ scripts/vc.py
@@ -62,5 +62,3 @@ if not os.path.exists('output/all_stats.csv'):
     all_stats = all_stats.merge(vtc[['annotation_filename', 'recording_filename']], how = 'left', left_on = 'annotation_filename', right_on = 'annotation_filename')
     all_stats = all_stats.merge(recordings[['filename', 'child_id', 'date_iso', 'min_duration']], how = 'left', left_on = 'recording_filename', right_on = 'filename')
     all_stats.to_csv('output/all_stats.csv', index = False)
-
-all_stats = pd.read_csv('output/all_stats.csv')
