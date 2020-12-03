1955028b24cf10a136a8b7028101e7cabe9489ac
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-12-03 18:47:41 +0100

    audio integrity

--- reports/compile.sh
+++ reports/compile.sh
@@ -1,1 +1,2 @@
 jupyter nbconvert "reports/Voice type classifier stability.ipynb" --to=pdf --TemplateExporter.exclude_input=True
+jupyter nbconvert "reports/Audio integrity.ipynb" --to=pdf --TemplateExporter.exclude_input=True
